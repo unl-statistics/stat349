9f339b011ffa5c6fba3f885e8e207a6597914726
Susan Vanderplas <srvanderplas@gmail.com>
2026-01-04 20:13:04 -0600

    Updates to syllabus and website

--- schedule.R
+++ schedule.R
@@ -5,62 +5,56 @@ library(lubridate)
 library(stringr)
 library(forcats)
 library(ggplot2)
-library(readxl)
 
-# Create a calendar for your syllabus 
+# Create a calendar for your syllabus
 # Source: http://svmiller.com/blog/2020/08/a-ggplot-calendar-for-your-semester/
 
-semester_info <- read_xlsx("course-schedule.xlsx", sheet = "SemesterDates")
-
 date_seq <- function(tbl) {
-  stopifnot("Start" %in% names(tbl))
-  stopifnot("End" %in% names(tbl))
-  
-  if(is.na(tbl$End)) {
-    ymd(tbl$Start)
+  stopifnot("start" %in% names(tbl))
+  stopifnot("end" %in% names(tbl))
+
+  if(is.na(tbl$end)) {
+    ymd(tbl$start)
   } else {
-    seq(ymd(tbl$Start), ymd(tbl$End), by = 1)
-  }       
+    seq(ymd(tbl$start), ymd(tbl$end), by = 1)
+  }
 }
 
-# Weekday(s) of class
-class_wdays <- semester_info |>
-  filter(Description == "Class") |>
-  select(Days) |>
-  mutate(Days = str_split(Days, ",", simplify = F)) |>
-  pluck("Days") |>
-  unlist() |>
-  str_trim()
 
+library(yaml)
+semester_info <- read_yaml("schedule.yaml")
+due_dates <- semester_info$`important-course-dates` |>
+  purrr::map_dfr(tibble::as_tibble) |>
+  dplyr::mutate(date = ymd(date), category = "due")
+
+schedule <- semester_info$schedule |>
+  purrr::map_dfr(tibble::as_tibble)
+
+dates <- purrr::map_dfr(semester_info$semester, as_tibble) |>
+  pivot_longer(everything(), names_to="type", values_to="date") |>
+  filter(!is.na(date)) |>
+  mutate(date=ymd(date))
+
+
+# Weekday(s) of class
+class_wdays <- semester_info$`class-days`
 
 # What are the full dates of the semester? Monday of week 1 - end of finals
-semester_dates <- semester_info |>
-  filter(Description == "Semester") |>
+semester_dates <- dates |>
+  filter(type %in% c("start", "end")) |>
+  pivot_wider(names_from="type", values_from="date") |>
   date_seq()
 
 # Dates that are school holidays/no class by university fiat
-not_here_dates <- semester_info |>
-  filter(Description == "Holiday") |>
-  select(Start, End) |>
-  mutate(id = 1:n()) |>
-  nest(data = -id) |>
-  mutate(dates = purrr::map(data, date_seq)) |>
-  unnest(dates) |>
-  pluck("dates")
+not_here_dates <- dates |>
+  filter(type == "holiday") |>
+  pluck("date")
 
-project_dates <- read_xlsx("course-schedule.xlsx", "due-dates") |>
-  mutate(date=ymd(date))
+exam_week <-  dates |>
+  filter(type=="exams") |>
+  pluck("date")
 
-exam_week <-  semester_info |>
-  filter(Description == "Exams") |>
-  select(Start, End) |>
-  mutate(id = 1:n()) |>
-  nest(data = -id) |>
-  mutate(dates = purrr::map(data, date_seq)) |>
-  unnest(dates) |>
-  pluck("dates")
-
-# Custom function for treating the first day of the month as the first week 
+# Custom function for treating the first day of the month as the first week
 # of the month up until the first Sunday (unless Sunday was the start of the month)
 wom <- function(date) {
   first <- wday(as.Date(paste(year(date),month(date),1,sep="-")))
@@ -73,13 +67,14 @@ Cal <- tibble(date = seq(floor_date(min(semester_dates), "month"), ceiling_date(
          wkdy = weekdays(date, abbreviate=T), # get weekday label
          wkdy = fct_relevel(wkdy, "Sun", "Mon", "Tue", "Wed", "Thu","Fri","Sat"), # make sure Sunday comes first
          semester = date %in% semester_dates, # is date part of the semester?
-         due = date %in% project_dates$date, # is it a due date?
+         due = date %in% due_dates$date, # is it a due date?
          not_here = date %in% not_here_dates, # is it a day off?
          exam_wk = date %in% exam_week,
          day = lubridate::mday(date), # get day of month to add later as a label
          # Below: our custom wom() function
          week = wom(date),
-         sem_week = pmin(17, pmax(0, epiweek(date) - 3)) - if_else(date > ymd(20250316), 1, 0))
+         sem_week=epiweek(date)) |>
+  mutate(sem_week = if_else(semester, pmax(0, sem_week-min(sem_week[semester]) + 1), NA))
 
 # Create a category variable, for filling in squares colorwise
 
@@ -92,13 +87,13 @@ Cal <- Cal %>%
     semester ~ "Semester",
     TRUE ~ "NA"
   )) |>
-  left_join(project_dates, by = c("date", "category"))
+  left_join(due_dates, by = c("date", "category"))
 
 class_cal <- ggplot(Cal, aes(wkdy, week)) +
   theme_bw() +
   theme(panel.grid.major.x = element_blank(),
         legend.position = "inside",
-        legend.position.inside = c(1,0), 
+        legend.position.inside = c(1,0),
         legend.justification = c(1, 0),
         legend.title = element_blank(),
         axis.title.y = element_blank(), axis.title.x = element_blank(), axis.ticks.y = element_blank(), axis.text.y = element_blank()) +
@@ -110,8 +105,8 @@ class_cal <- ggplot(Cal, aes(wkdy, week)) +
   # put your y-axis down, flip it, and reverse it
   scale_y_reverse(breaks=NULL) +
   # manually fill scale colors to something you like...
-  scale_color_manual(values = c("FALSE" = "grey70", "TRUE" = "black"), guide = "none") + 
-  scale_fill_manual(values=c("Class"="purple", 
+  scale_color_manual(values = c("FALSE" = "grey70", "TRUE" = "black"), guide = "none") +
+  scale_fill_manual(values=c("Class"="purple",
                              "Due date"="orange",
                              "Semester"="white",
                              "Finals" = "grey70",
@@ -122,18 +117,19 @@ class_cal <- ggplot(Cal, aes(wkdy, week)) +
                     breaks=c("Semester", "UNL holiday", "Due date", "Class", "Finals"))
 # class_cal
 
-topics <- read_excel("course-schedule.xlsx",  sheet = "Week-plan") |>
-  rename(sem_week=Week, topic = Title) |>
-  select(sem_week, topic)
+topics <- schedule |>
+  mutate(sem_week=1:n()) |>
+  select(sem_week, name)
 
 
-duedates <- filter(Cal, category == "Due date") |>
-  mutate(important = paste(topic, ": ", format.Date(date, "%b %d"), sep = "")) |>
+duedates <- due_dates |>
+  left_join(select(Cal, date, sem_week), by = "date") |>
+  mutate(important = name) |>
   select(sem_week, important)
 
 schedule <- topics |>
   full_join(duedates) |>
   arrange(sem_week) |>
-  rename("Week" = sem_week, "Topic" = topic, "Important Dates" = important)
+  rename("Week" = sem_week, "Topic" = name, "Important Dates" = important)
 
 # schedule
